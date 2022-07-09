ad35d41acd94cfda5c624d2e7f027c2b845d1ded
Murty Gudipati <murty.gudipati@saven.in>
2022-07-09 18:32:44 +0530

    Update streamlit_app.py
    control flow debugging.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -36,6 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # display the table
 streamlit.dataframe(fruityvice_normalized)
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
