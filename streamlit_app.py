16861698ef1e2b36d1c8df4fa4cd70bf95769637
Murty Gudipati <murty.gudipati@saven.in>
2022-07-09 16:01:56 +0530

    Update streamlit_app.py
    separate the base URL from the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 
 # normalize the json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
