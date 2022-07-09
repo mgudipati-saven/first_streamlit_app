351cd3a68e7059b2567998964e1bc8af8abde561
Murty Gudipati <murty.gudipati@saven.in>
2022-07-09 15:59:56 +0530

    Update streamlit_app.py
    pretty table for fruity advice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# normalize the json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# display the table
+streamlit.dataframe(fruityvice_normalized)
